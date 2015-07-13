cc4ad0d542ff8f5af59830fce5521334968ef5ba
Guanglei BAO <guanglei@amazon.com>
2015-07-13 11:23:34 +0800

    Adding tsunamiEnable.sh.

---
+++ tsunamiEnable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# NO PRODUCTION SAFE WARRANTY!!
+# For EC2 using single security group on eth0. You can modify it to work in more complex case.
+# Clone https://github.com/sebsto/tsunami-udp.git under ~/git directory.
+# Comment out ./distribute.sh in bootstrap.sh first.
+
+BSSGID=<sg-id-your-ec2-currently-using>
+TMSGID=<sg-id-allowing-tsunami-traffic>
+IID=`GET http://169.254.169.254/latest/meta-data/instance-id`
+MAC=`GET http://169.254.169.254/latest/meta-data/mac`
+INFID=`GET http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC}/interface-id`
+aws ec2 modify-network-interface-attribute --network-interface-id ${INFID} --groups ${BSSGID} ${TMSGID}
+sudo /home/ec2-user/git/tsunami-udp/AWS/bootstrap.sh
+cd /usr/local/tsunami-udp/
+sudo make install
